a153fb87a7caa5307732d0f52ee0c4ecdb55a43a
Cz4js0n <119066425+Cz4js0n@users.noreply.github.com>
2023-03-22 16:03:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 var suma = 0;
 var ilosc = 0;
-	function zmiana(komorka) {
-		if (komorka.style.backgroundColor === "lightyellow") 
-        {
-			suma -= parseInt(komorka.innerText);
-            ilosc -= 1;
-			komorka.innerText = "";
-			komorka.style.backgroundColor = "#FFBFA9";
-		} 
-        else 
-        {
-            var nowa = prompt("Podaj wartość");
-            komorka.innerText = nowa;
-			suma += parseInt(komorka.innerText);
-            ilosc += 1;
-			komorka.style.backgroundColor = "lightyellow";
-		}
-	}
-	function sumowanie() 
-    {
-		alert("Suma klikniętych komórek wynosi " + suma);
-	}
-    function sredniaa()
-    {
-        var srednia = suma/ilosc;
-        alert("Średnia klikniętych komórek wynosi " + srednia);
-    }
-    var miejsca = document.querySelectorAll('td');
-    miejsca.forEach(miejsce => {
-        miejsce.addEventListener('click', function() {
-            zmiana(this);
-        });
+var miejsca = document.querySelectorAll('td');
+miejsca.forEach(miejsce => {
+    miejsce.addEventListener('click', function() {
+        zmiana(miejsce);
     });
+});
+function zmiana(komorka) {
+	if (komorka.style.backgroundColor === "lightyellow") 
+    {
+		suma -= parseInt(komorka.innerText);
+        ilosc -= 1;
+		komorka.innerText = "";
+		komorka.style.backgroundColor = "#FFBFA9";
+	} 
+    else 
+    {
+        var nowa = prompt("Podaj wartość");
+        komorka.innerText = nowa;
+		suma += parseInt(komorka.innerText);
+        ilosc += 1;
+		komorka.style.backgroundColor = "lightyellow";
+	}
+}
+function sumowanie() {
+	alert("Suma klikniętych komórek wynosi: " + suma);
+}
+function sredniaa() {
+    var srednia = suma/ilosc;
+    alert("Średnia klikniętych komórek wynosi: " + srednia);
+}
